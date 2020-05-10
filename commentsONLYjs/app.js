5d2b120dc20eae86f71b2cc4de3130e03dfcef74
Jancy Baez <rookiewrist@Jancys-MacBook-Air.local>
2020-05-10 16:52:49 -0400

    message

--- commentsONLYjs/app.js
+++ commentsONLYjs/app.js
@@ -7,7 +7,19 @@
 /* Function to GET Web API Data*/
 
 /* Function to POST data */
+app.post('/addAnimal', addAnimal);
 
+function addAnimal(req,res){
+
+  newEntry = {
+    animal: req.body.animal,
+    facts: req.body.fact,
+    fav: req.body.fav
+  }
+
+  animalData.push(newEntry)
+  console.log(animalData)
+}
 
 /* Function to GET Project Data */
 
