5df9ced9c0b8a7f1567556f0ea8ec6cdec3b4096
Jancy Baez <rookiewrist@Jancys-MacBook-Air.local>
2020-05-10 13:43:37 -0400

    first commit

---
+++ commentsONLYjs/server.js
@@ -0,0 +1,29 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {}
+
+// Require Express to run server and routes
+const express = required ('express');
+const bodyParser = required ('body-Parser');
+
+// Start up an instance of app
+const app = express ();
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = required ('cors');
+app.use(cors())
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+//port variable
+const port = 6000;
+
+// Setup Server
+const server = app.listen(port, listening);
+function listening(){
+    console.log(`running localhost: ${port}`);
+};
