e1f65455381046ab1fbef5f0cb07aa39b0c2ee12
Jancy Baez <rookiewrist@Jancys-Air.hsd1.ma.comcast.net>
2020-05-13 01:21:04 -0400

    first commit

--- website/.app.js
+++ website/.app.js
@@ -1,22 +1,27 @@
 /* Global Variables */
-const URL = 'hhtps://api.openweathermap.org/data/2.5/weather?zip=';
-const keyAPI = ''
+const Url = 'hhtps://api.openweathermap.org/data/2.5/weather?zip=';
+const keyApi = ''
+
+//HTML variables for events
+const button = document.getElementById('generate');
+
+//HTML elements to update
+const zipCode = document.getElementById('zip');
+const feelings = document.getElementById('feelings')
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 
-const postData = async ( URL, data, keyAPI = {})
-
-function {
-    console.log(projectData);
-      const response = await fetch(url, {
+const weatherData = async ( Url, areaCode, keyAPI ) => {
+      const response = await fetch(Url + areaCode + keyApi, {
       method: 'POST', 
       credentials: 'same-origin',
       headers: {
           'Content-Type': 'application/json',
-      },
+      }),
 
         body: JSON.stringify(projectData), 
     });
@@ -25,7 +30,23 @@ function {
         const newData = await  response.json();
         console.log(newData);
         return newData;
-      }catch(error) {
+      } catch(error) {
       console.log("error", error);
       }
   }
+
+  button.addEventListener ('click',   
+  getWeather(url, zip.value, keyApi)
+  .then(temp => {
+    return {date: newDate, temp, content: feelings.value}
+  })
+  .then(data => {
+    saveData('/api/projectdata', data)
+    return data
+  })
+  .then(({temp, date, content}) => updateUI(temp, date, content))
+  .catch(e => {
+    // There can be proper error handling with UI
+    console.error(e)
+  })
+})
